6d2e5575bccb4e36257cda2ea75d1308fb9727e7
Zhurbey Aleksandr <AZhurbey@bell-main.bellintegrator.ru>
2019-06-25 18:56:32 +0400

    Подготовка проекта

--- easy-chat-web/src/conteiners/App/App.tsx
+++ easy-chat-web/src/conteiners/App/App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import'./App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="container">
-   Приложение тут
+      <div className="container headText">
+   <Container>
+     <Row>
+       <Col sm="6">666</Col>
+       <Col sm="6">555</Col>
+     </Row>
+   </Container>
       </div>
     );
   }
