ad22b09d39f1088b622aed91910606c67b83a40d
Aleksander Zhurbey <azhurbey@bellintegrator.com>
2019-06-26 09:13:20 +0400

    Дизайн формы

--- easy-chat-web/src/conteiners/App/components/LoginForm.tsx
+++ easy-chat-web/src/conteiners/App/components/LoginForm.tsx
@@ -5,12 +5,14 @@ import './LoginForm.css'
 class LoginForm extends React.Component {
   public render() {
     return (
+      <div>
         <form>
-       <input className="mainLoginInput" type="text" placeholder="Логин" name="name" />
-       <input className="mainLoginInput" type="password" placeholder="Пароль" name="name" />
-       <br/><br/>
-       <input type="submit" className="mainLoginButton" value="Войти" />
-       </form>
+          <div className="form-group">
+            <input type="text" className="form-control mainLoginInput" id="login" placeholder="Логин" />
+            <input type="password" className="form-control mainLoginInput" id="password" placeholder="Пароль" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
