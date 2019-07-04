16370faa3f980803d4ceaf47b7c74a7031e58069
Zhurbey Aleksandr <AZhurbey@bell-main.bellintegrator.ru>
2019-07-04 17:17:18 +0400

    Дизайн формы входа

--- easy-chat-web/src/conteiners/App/components/LoginForm.tsx
+++ easy-chat-web/src/conteiners/App/components/LoginForm.tsx
@@ -14,13 +14,13 @@ function LoginForm ({submitLogin, onChangeForm}:IProps) {
             <input type="text"
              onChange={onChangeForm} 
              className="form-control mainLoginInput"
-              id="login" placeholder="Логин"
+              id="login" placeholder="логин | телефон"
                />
             <input 
             type="password"
             onChange={onChangeForm}
              className="form-control mainLoginInput"
-              id="password" placeholder="Пароль" />
+              id="password" placeholder="пароль" />
             <button onClick={submitLogin} type="button" className="btn btn-dark mainLoginButton">Easy Go </button>
           </div>
         </form>
